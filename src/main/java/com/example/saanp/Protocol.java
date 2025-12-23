6a80be5737c8821bd7bec6be5536dc6bb459c12d
Piyush490 <piyushagarwal490@gmail.com>
2025-12-23 23:19:28 +0530

    a

--- src/main/java/com/example/saanp/Protocol.java
+++ src/main/java/com/example/saanp/Protocol.java
@@ -1,28 +1,55 @@
 package com.example.saanp;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 
 public class Protocol {
 
     public static void broadcast(GameRoom room) {
-        StringBuilder sb = new StringBuilder();
 
-        sb.append("P|");
+        JsonObject root = new JsonObject();
+        root.addProperty("type", "snapshot");
+
+        JsonObject data = new JsonObject();
+
+        // Players
+        JsonArray players = new JsonArray();
         for (Player p : room.getPlayers()) {
-            sb.append(p.snake.x).append(",")
-                    .append(p.snake.y).append(",")
-                    .append(p.snake.radius).append(";");
+
+            JsonObject po = new JsonObject();
+            po.addProperty("id", p.channel.id().asShortText());
+            po.addProperty("x", p.snake.x);
+            po.addProperty("y", p.snake.y);
+            po.addProperty("angle", p.snake.angle);
+            po.addProperty("radius", p.snake.radius);
+            po.addProperty("dead", p.snake.dead);
+            po.addProperty("color", p.color);
+
+            players.add(po);
         }
 
-        sb.append("|F|");
+        // Food
+        JsonArray food = new JsonArray();
         for (Food f : room.getFoods()) {
-            sb.append(f.x).append(",")
-                    .append(f.y).append(";");
+
+            JsonObject fo = new JsonObject();
+            fo.addProperty("x", f.x);
+            fo.addProperty("y", f.y);
+
+            food.add(fo);
         }
 
-        String msg = sb.toString();
+        data.add("players", players);
+        data.add("food", food);
+
+        root.add("data", data);
+
+        String json = root.toString();
+
+        // Broadcast to all players
         for (Player p : room.getPlayers()) {
-            p.channel.writeAndFlush(new TextWebSocketFrame(msg));
+            p.channel.writeAndFlush(new TextWebSocketFrame(json));
         }
     }
 }
