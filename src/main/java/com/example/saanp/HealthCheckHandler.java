af835d999c52dcbd70f9d9f36bc0a188efd5f9cf
Piyush490 <piyushagarwal490@gmail.com>
2025-12-25 00:44:52 +0530

    a

--- src/main/java/com/example/saanp/HealthCheckHandler.java
+++ src/main/java/com/example/saanp/HealthCheckHandler.java
@@ -1,26 +1,21 @@
 package com.example.saanp;
 
-import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.*;
-
-import static io.netty.handler.codec.http.HttpHeaderNames.CONTENT_LENGTH;
-import static io.netty.handler.codec.http.HttpHeaderNames.CONTENT_TYPE;
-import static io.netty.handler.codec.http.HttpResponseStatus.OK;
-import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
 
 public class HealthCheckHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, FullHttpRequest req) {
+        // Simple health check for Render/Cloud deployment
         if ("/".equals(req.uri())) {
-            FullHttpResponse res =
-                    new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
+            FullHttpResponse res = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
             ctx.writeAndFlush(res).addListener(ChannelFutureListener.CLOSE);
         } else {
-            ctx.fireChannelRead(req.retain()); // 🔥 REQUIRED
+            // Pass the request to the next handler (WebSocket)
+            ctx.fireChannelRead(req.retain());
         }
     }
 }
-
